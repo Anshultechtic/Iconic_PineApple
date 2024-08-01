33b0bb763caf22a0518b923e7670646296f64ae8
Anshultechtic <139873271+Anshultechtic@users.noreply.github.com>
2024-08-01 18:38:45 +0530

    kkk

--- src/main/java/utilities/ExcelWriteData.java
+++ src/main/java/utilities/ExcelWriteData.java
@@ -18,26 +18,28 @@ import com.lowagie.text.Row;
 
 public class ExcelWriteData {
 
+	static String path = System.getProperty("user.dir") + "\\src\\test\\resources\\ExcelSheets\\Standard_Sheet_9.xlsx";
+
 	public static void main(String[] args) {
 
 		try {
-			writeDataInExcel3("contacts", "Email", createEmail());
-			writeDataInExcel3("contacts", "First_Name", firstName());
-			writeDataInExcel3("contacts", "Last_Name", lastName());
-			writeDataInExcel3("contacts", "Phone_Number", phoneNumber());
+			writeDataInExcel3("contacts", "Email", createEmail(50000));
+			writeDataInExcel3("contacts", "First_Name", firstName(50000));
+			writeDataInExcel3("contacts", "Last_Name", lastName(50000));
+			writeDataInExcel3("contacts", "Phone_Number", phoneNumber(50000));
+			writeDataInExcel3("contacts", "Date_Of_Birth", DOB(50000));
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
-//		System.out.println(phoneNumber());
 	}
 
-	public static List<String> createEmail() {
+	public static List<String> createEmail(int noOfRows) {
 
 		List<String> emailList = new ArrayList<>();
 
-		for (int i = 1; i < 10000; i++) {
+		for (int i = 1; i <= noOfRows; i++) {
 
 			if (i % 2 == 0) {
 				emailList.add("User" + i + "@yopmail.com");
@@ -54,11 +56,11 @@ public class ExcelWriteData {
 
 	}
 
-	public static List<String> firstName() {
+	public static List<String> firstName(int noOfRows) {
 
 		List<String> emailList = new ArrayList<>();
 
-		for (int i = 1; i < 10000; i++) {
+		for (int i = 1; i <= noOfRows; i++) {
 			emailList.add("UserFName_" + i);
 
 		}
@@ -67,11 +69,11 @@ public class ExcelWriteData {
 
 	}
 
-	public static List<String> lastName() {
+	public static List<String> lastName(int noOfRows) {
 
 		List<String> emailList = new ArrayList<>();
 
-		for (int i = 1; i < 10000; i++) {
+		for (int i = 1; i <= noOfRows; i++) {
 
 			emailList.add("UserLName_" + i);
 		}
@@ -80,11 +82,11 @@ public class ExcelWriteData {
 
 	}
 
-	public static List<String> phoneNumber() {
+	public static List<String> phoneNumber(int noOfRows) {
 
 		List<String> phoneList = new ArrayList<>();
 
-		for (int i = 1; i < 10000; i++) {
+		for (int i = 1; i <= noOfRows; i++) {
 			Random random = new Random();
 
 			// Generate the first digit (should be between 6 and 9)
@@ -102,42 +104,44 @@ public class ExcelWriteData {
 
 	}
 
-//	public static List<String> DOB() {
-//
-//		Random random = new Random();
-//
-//		// Generate a random year between 1900 and 2023
-//		int year = 1900 + random.nextInt(2023 - 1900 + 1);
-//
-//		// Generate a random month between 1 and 12
-//		int month = 1 + random.nextInt(12);
-//
-//		// Generate a random day based on the month and whether it is a leap year
-//		int day;
-//		if (month == 2) {
-//			// Check if the year is a leap year
-//			boolean isLeapYear = java.time.Year.isLeap(year);
-//			day = 1 + random.nextInt(isLeapYear ? 29 : 28);
-//		} else if (month == 4 || month == 6 || month == 9 || month == 11) {
-//			day = 1 + random.nextInt(30);
-//		} else {
-//			day = 1 + random.nextInt(31);
-//		}
-//
-//		// Create a LocalDate object with the generated year, month, and day
-//		LocalDate randomDate = LocalDate.of(year, month, day);
-//
-//		// Format the LocalDate object into DD MM YYYY format
-//		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MM yyyy");
-//
-//	}
-//
-//}
+	public static List<String> DOB(int noOfRows) {
+		List<String> dateList = new ArrayList<String>();
+
+		for (int i = 1; i <= noOfRows; i++) {
+			Random random = new Random();
+
+			// Generate a random year between 1900 and 2023
+			int year = 1900 + random.nextInt(2023 - 1900 + 1);
+
+			// Generate a random month between 1 and 12
+			int month = 1 + random.nextInt(12);
+
+			// Generate a random day based on the month and whether it is a leap year
+			int day;
+			if (month == 2) {
+				// Check if the year is a leap year
+				boolean isLeapYear = java.time.Year.isLeap(year);
+				day = 1 + random.nextInt(isLeapYear ? 29 : 28);
+			} else if (month == 4 || month == 6 || month == 9 || month == 11) {
+				day = 1 + random.nextInt(30);
+			} else {
+				day = 1 + random.nextInt(31);
+			}
+
+			// Create a LocalDate object with the generated year, month, and day
+			LocalDate randomDate = LocalDate.of(year, month, day);
+
+			// Format the LocalDate object into DD MM YYYY format
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-yyyy");
+			dateList.add(formatter.format(randomDate));
+//			System.out.println(formatter.format(randomDate));
+		}
+
+		return dateList;
+	}
 
 	public static void writeDataInExcel3(String sheetName, String columnName, List<String> dataToWrite)
 			throws IOException {
-
-		String path = System.getProperty("user.dir") + "\\src\\test\\resources\\ExcelSheets\\Standard_Sheet_07.xlsx";
 
 		FileInputStream fis = new FileInputStream(path);
 		XSSFWorkbook wb = new XSSFWorkbook(fis);
@@ -162,7 +166,7 @@ public class ExcelWriteData {
 			if (row1 == null) {
 				row1 = sh.createRow(i);
 			}
-			System.out.println("Row Number is " + (i + 1));
+//			System.out.println("Row Number is " + (i + 1));
 			Cell cell = row1.createCell(column);
 			cell.setCellValue(dataToWrite.get(i));
 
@@ -172,7 +176,7 @@ public class ExcelWriteData {
 		wb.write(fos);
 		fos.close();
 		fis.close(); // Close the FileInputStream after you're done using it
-		System.out.println("Data written successfully");
+		System.out.println(columnName+" written successfully");
 	}
 
 	public static void writeDataInExcel(String sheetName, String columnName, String dataToWrite) throws IOException {
